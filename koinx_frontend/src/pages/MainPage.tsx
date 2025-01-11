3e608fae3a19729d0b465f02ce10ef3ff718d18b
Chhavesh <chhaveshpatle@gmail.com>
2025-01-11 18:44:41 +0530

    Added Performance card

--- koinx_frontend/src/pages/MainPage.tsx
+++ koinx_frontend/src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import Btc from "../components/Btc";
 import WidgetContainer from "../components/WidgetContainer";
 import Toggle from "../components/Toggle.tsx";
+import Performance from "../components/Performance.tsx";
 function MainPage() {
   return (
     <div className="h-full w-screen bg-slate-200/40">
@@ -9,6 +10,7 @@ function MainPage() {
         <div className="lg:w-8/12 lg:ml-14 mx-4">
            <Btc />
            <Toggle/>
+           < Performance />
         </div>
 
     </div>
