7eb8c9d1000adf3963cbf5ecd8bbc8899ccc3396
Chhavesh <chhaveshpatle@gmail.com>
2025-01-11 17:04:29 +0530

    Added BTC

--- koinx_frontend/src/App.tsx
+++ koinx_frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar"
+import MainPage from "./pages/MainPage"
 
 function App() {
   return (
     <div>
        <Navbar/>
+       <MainPage/>
     </div>
   )
 }
---
+++ koinx_frontend/src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import Btc from "../components/Btc";
+function MainPage() {
+  return (
+    <div>
+      <Btc />
+    </div>
+  )
+}
+
+export default MainPage
